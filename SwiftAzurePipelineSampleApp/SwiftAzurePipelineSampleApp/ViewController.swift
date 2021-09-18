0f9a5bb0dbd1a0400018e30c92904eaeb2c603d7
Aviral Aggarwal <Aviral190694@users.noreply.github.com>
2021-09-18 20:08:37 +0530

    First Commit with sample project

---
+++ SwiftAzurePipelineSampleApp/SwiftAzurePipelineSampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftAzurePipelineSampleApp
+//
+//  Created by Aviral Aggarwal on 18/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
